1fc5ac5b150dd208b35d19dcf4f553e44e9dc864
Philip <popoolaphilip10@gmail.com>
2026-06-24 22:18:47 +0100

    chore: initialize project dependencies and node_modules directory

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Urbanist", "sans-serif"],
+      },
+      colors: {
+        primary: "#2563EB",
+      },
+    },
+  },
+  plugins: [],
+};
